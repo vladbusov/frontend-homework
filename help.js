ff55d8c8bb4465738730065ba0739b428598ed6d
Anatoliy Ostapenko <a.ostapenko@corp.mail.ru>
2018-02-02 02:25:17 +0300

    Поправил скрипт публикации ДЗ

--- help.js
+++ help.js
@@ -4,7 +4,7 @@ const co = require('co');
 const promisify = require('promisify-node');
 const exec = require('child_process').exec;
 const fs = promisify('fs');
-const help = require('./help');
+const descs = require('./descs');
 
 const e = function (cmd) {
 	return new Promise(function (resolve, reject) {
@@ -20,14 +20,8 @@ const e = function (cmd) {
 	});
 };
 
-const greet = n => `# frontend-homework-${n}
-#### Simple homework for students №${n}
 
-В этом репозитории содержится условие варианта №${n} домашнего задания. Чтобы начать с ним работать, вам необходимо склонировать репозиторий к себе на компьютер и перейти в ветку своего варианта.
-`;
-
-
-const createVariant = function *({desc:description, name, number}, names, readme, index) {
+const createVariant = function *({ desc: description, name, number }, names, readme, index) {
 	console.log(`\n\n\n####################################`);
 	console.log(`Process variant-${number}`);
 
@@ -37,16 +31,16 @@ const createVariant = function *({desc:description, name, number}, names, readme
 	yield e(`git checkout -b variant-${number}`);
 
 	yield fs.writeFile('index.html', index
-		.replace(`<title>Homework №</title>`, `<title>Homework №${number}</title>`)
-		.replace(`<script src="tests/name.js"></script>`, `<script src="tests/${name}.js"></script>`)
+		.replace(/{{{VAR}}}/ig, number.toString)
+		.replace(/{{{NAME}}}/ig, name),
 	);
 	yield fs.writeFile('README.md', readme
-		.replace('{{{greet}}}', greet(number))
-		.replace('{{{task}}}', description)
+		.replace(/{{{VAR}}}/ig, number.toString)
+		.replace('{{{TASK}}}', description),
 	);
 
 	const filesToRemove = names
-		.filter(n => n !== name)
+		.filter(n => ![name, 'max'].includes(n))
 		.map(n => `tests/${n}.js`)
 		.join(' ');
 
@@ -54,36 +48,29 @@ const createVariant = function *({desc:description, name, number}, names, readme
 
 
 	yield e('rm ' + filesToRemove);
-	yield e('rm descs.txt');
+	yield e('rm descs.js');
 	yield e('rm help.js');
-	yield e('rm -rf node_modules/');
-	yield e('rm tests/merge.js');
+	// yield e('rm -rf node_modules/');
 
 	yield e(`git add .`);
 	yield e(`git add --all`);
-	yield e(`git commit -am "Init variant ${number}"`);
+	yield e(`git commit -am "Подготовка варианта ${number}"`);
 	yield e(`git push -u origin variant-${number}:variant-${number}`);
 };
 
 co(function *() {
-	const descsSource = yield fs.readFile('descs.txt', 'utf-8');
 	const readme = yield fs.readFile('README.md', 'utf-8');
 	const index = yield fs.readFile('index.html', 'utf-8');
-	const descs = descsSource
-		.split('\n')
-		.filter(s => s)
-		.map(s => s.trim().slice(2))
-		.sort();
 
 	const names = [];
 
 	const variants = descs.map((desc, number) => {
-		const name = desc.match(/`(.+)`/i)[1];
+		const name = desc.match(/`(.+)`/i)[ 1 ];
 		names.push(name);
-		return {name, desc, number: number + 1};
+		return { name, desc, number: number + 1 };
 	});
 
 	for (const variant of variants) {
-		yield * createVariant(variant, names, readme, index);
+		yield *createVariant(variant, names, readme, index);
 	}
 }).catch(console.error);
