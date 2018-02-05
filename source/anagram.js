f54a7b14f7367fd913c2ad9627ed2834ff59e3d6
Vlad Busov <to2002to2002@gmail.com>
2018-02-05 13:54:59 +0300

    переделал функционал из Object в Map
    тут

--- source/anagram.js
+++ source/anagram.js
@@ -1,47 +1,33 @@
 'use strict';
 
 /**
- * Получения массив уникальных слов из произвольного массива слов
- * @param   {string[]} arr
- * @returns {string[]}
- */
-
-const unique = arr => {
-	let map = new Map();
-	let output = new Array();
-	arr.forEach(function(item, i, array) { 
-		if (!map.has(item)) { 
-			map.set(item, true); 
-			output.push(item); 
-		} 
-	});
-	return output;
-}
-
-/**
  * Получения двумерный массив анаграмм из произвольного массива слов
  * @param   {string[]} input
  * @returns {Array<[string, string]>}
  */
-
 const anagram = input => {
 	let anagrams = new Map();
-	input.forEach( function(word, i, array) {
+	input.forEach( word => {
 		word = word.toLowerCase();
 		var sorted = word.split("").sort().join();
-		if (anagrams[sorted] != null) { 
-			anagrams[sorted].push(word);
-		} else { 
-			anagrams[sorted] = [ word ];
-		}; 
-	});
 
+		if (anagrams.has(sorted)) {
+			let list = anagrams.get(sorted);
+			list.push(word);
+			anagrams.set(sorted,list);
+		} else { 
+			anagrams.set(sorted, new Array(word) );
+		}; 
+		
+	});
+	
 	let output = new Array();
-	for (let group of Object.values(anagrams) ){
-		group = unique(group);
+	for (let group of anagrams.values() ){
+		group = Array.from(new Set(group));
 		if ( group.length > 1 ){ 
 			output.push( group.sort() ); 
 		}
 	}
+
 	return output.sort(); 
 };
