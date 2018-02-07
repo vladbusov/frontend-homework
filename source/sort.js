71a54b999d8b68d17e5151b8d2ff3f0a3a4b5865
Vlad Busov <to2002to2002@gmail.com>
2018-02-07 14:26:23 +0300

    комментарий удалил я
    да

--- source/sort.js
+++ source/sort.js
@@ -2,7 +2,6 @@
 
 const sort = str => {
 	const uniSort = (a, b) => Intl.Collator().compare(a, b);
-	//const arr = str.split(' ');
 	return str.split(' ').map( word => {
 		let output = word.split('').sort(uniSort).join('');
 		output = output.charAt(0).toUpperCase() + output.substr(1).toLowerCase();
