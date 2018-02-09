a0d2d80c4ba7468ecc2d825e11ea6730318241a4
Vlad Busov <to2002to2002@gmail.com>
2018-02-09 12:07:50 +0300

    исправил
    spread оператор

--- source/sort.js
+++ source/sort.js
@@ -3,8 +3,7 @@
 const sort = str => {
 	const uniSort = (a, b) => Intl.Collator().compare(a, b);
 	return str.split(' ').map( word => {
-		// ((...args) => args)(...str) так ?
-		let output = word.split('').sort(uniSort).join('');
+		let output = [...word].sort(uniSort).join('');
 		output = output.charAt(0).toUpperCase() + output.substr(1).toLowerCase();
 		return output;
 	}).sort(uniSort).join(' ');
